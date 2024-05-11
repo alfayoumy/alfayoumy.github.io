5120b95d1028c50ae9d3976668085510aa5d22f2
alfayoumy <amr.alfayoumi@gmail.com>
2024-05-11 03:34:23 +0300

    layout

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -8,14 +8,7 @@
             }
         },
         initialize = function() {
-            // Check if the device is a desktop or mobile
-            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                // For mobile devices
-                changeViewportContent("width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
-            } else {
-                // For desktop devices
-                changeViewportContent("width=device-width, initial-scale=1.5");
-            }
+            changeViewportContent("width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
         },
         gestureStart = function() {
             changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
@@ -25,10 +18,9 @@
         };
 
 
-    initialize();
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        initialize();
         document.addEventListener("touchstart", gestureStart, false);
         document.addEventListener("touchend", gestureEnd, false);
     }
-    
 })(document);
